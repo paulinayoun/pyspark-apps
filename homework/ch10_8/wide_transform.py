ac4ce675f87a7477dcdb28a8f93aa81a260c6876
HyeJeong Yoon <143067143+paulinayoun@users.noreply.github.com>
2025-11-12 14:18:38 +0900

    ch10_8 테스트

---
+++ homework/ch10_8/wide_transform.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, broadcast
+from time import sleep
+
+spark = SparkSession \
+    .builder \
+    .appName("wide_transform") \
+    .config("spark.sql.adaptive.enabled", "false") \
+    .config("spark.executor.cores", "2") \
+    .config("spark.executor.memory", "2g") \
+    .config("spark.executor.instances", "3") \
+    .getOrCreate()
+
+print("=== DataFrame app Start ===")
+
+
+jobs_df = "hdfs:///home/spark/sample/linkedin_jobs/jobs/job_skills.csv"
+jobs_schema = 'job_id LONG, skill_abr STRING'
+skills_df = "hdfs:///home/spark/sample/linkedin_jobs/mappings/skills.csv"
+skills_schema = 'skill_abr STRING, skill_name STRING'
+
+skills_df = spark.read \
+    .option("header", "true") \
+    .option('multiLine', 'true') \
+    .schema(skills_schema) \
+    .csv(skills_df)
+
+jobs_df = spark.read \
+    .option("header", "true") \
+    .option('multiLine', 'true') \
+    .schema(jobs_schema) \
+    .csv(jobs_df)
+
+# 조인 수행 전 확인
+jobs_df.persist()
+jobs_df.show()
+
+# 조인 수행, skills DataFrame을 broadcast 처리
+join_df = jobs_df.join(
+    other = broadcast(skills_df),
+    on = 'skill_abr')
+
+# 최종 데이터프레임 컬럼 ['skill_name','job_count'] 기준 내림차순
+result_df = join_df.groupBy('skill_name') \
+    .count() \
+    .withColumnRenamed('count', 'job_count') \
+    .sort(col('job_count').desc())
+
+result_df.show(20)
+
+time.sleep(1200)
