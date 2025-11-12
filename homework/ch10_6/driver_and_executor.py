2d56c981e123e79050b85d7804b93e98e70cc4b9
HyeJeong Yoon <143067143+paulinayoun@users.noreply.github.com>
2025-11-12 12:54:14 +0900

    ch10_6 driver_executor 경로 수정

--- homework/ch10_6/driver_and_executor.py
+++ homework/ch10_6/driver_and_executor.py
@@ -12,7 +12,7 @@ spark = SparkSession \
 
 print("=== DataFrame app Start ===")
 
-postings = "hdfs:///home/spark/sample/linkedin_jobs/companies/postings.csv"
+postings = "hdfs:///home/spark/sample/linkedin_jobs/postings.csv"
 postings_schema = 'job_id LONG, company_name STRING, title STRING, description STRING, max_salary LONG, pay_period STRING, location STRING, company_id LONG, views LONG, med_salary LONG, min_salary LONG, formatted_work_type STRING, applies LONG, original_listed_time TIMESTAMP, remote_allowed STRING, job_posting_url STRING, application_url STRING, application_type STRING, expiry STRING, closed_time TIMESTAMP, formatted_experience_level STRING, skills_desc STRING, listed_time TIMESTAMP, posting_domain STRING, sponsored LONG, work_type STRING, currency STRING, compensation_type STRING'
 
 postings_df = spark.read \
